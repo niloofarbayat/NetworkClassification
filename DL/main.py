a8435b086caeef27ddeda0bc62bd3ec0aedbf8c2
Niloofar Bayat <niloofarbayat@dyn-160-39-57-56.dyn.columbia.edu>
2018-11-16 00:58:06 -0500

    added auto-sklearn to DL and cleaned up

--- DL/main.py
+++ DL/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.metrics import accuracy_score
 from keras.preprocessing import text, sequence
 from keras.preprocessing.text import Tokenizer
 from keras.utils import to_categorical
@@ -15,6 +16,13 @@ from sklearn.model_selection import KFold
 from keras.callbacks import History
 import matplotlib.pyplot as plt
 #from attention_decoder import AttentionDecoder
+import autosklearn.classification
+
+
+BATCH_SIZE = 32
+EPOCHS = 10
+FOLDS = 10   
+
 
 def read_csv(file_path, has_header=True):
     with open(file_path) as f:
@@ -51,27 +59,6 @@ def data_load_and_filter(datasetfile, min_connections):
     print("Filtered shape of X =", np.shape(X))
     print("Filtered shape of y =", np.shape(y))   
 
-    return X, y
-
-
-def create_model(time_steps, n_features, n_labels):
-    model = Sequential()
-
-    model.add(GRU(100+n_labels, return_sequences=True, input_shape=(time_steps, n_features)))
-    model.add(GRU(100+n_labels))
-    model.add(Dense(n_labels, activation='softmax'))
-    model.compile(loss='sparse_categorical_crossentropy',optimizer='adam', metrics=['acc'])
-    model.summary()
-
-    return model  
-
-#########################################################
-###### USE RNN TO CLASSIFY PACKET SEQUENCES -> SNI ######
-#########################################################
-
-def DLClassification(datasetfile, min_connections):
-    X, y = data_load_and_filter(datasetfile, min_connections)
-
     ##### BASIC PARAMETERS #####
     n_samples = np.shape(X)[0]
     time_steps = np.shape(X)[1] # we have a time series of 100 payload sizes
@@ -103,43 +90,89 @@ def DLClassification(datasetfile, min_connections):
     X = sequences
 
     ##### RESHAPE FOR LSTM #####
-    X = np.reshape(X, (n_samples, time_steps, n_features))
+    #X = np.reshape(X, (n_samples, time_steps, n_features))
+    return X, y, time_steps, n_features, n_labels
 
-    BATCH_SIZE = 32
-    EPOCHS = 20
-    FOLDS = 10
+  
+  
+def create_model(time_steps, n_features, n_labels):
+    model = Sequential()
 
-    kf = KFold(n_splits=FOLDS, shuffle=True)
-    total = 0
-    for train_index, test_index in kf.split(X):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        history = History()
-        
-        model = create_model(time_steps, n_features, n_labels)
-        model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=1, shuffle=False, validation_data=(X_test, y_test), callbacks = [history])
-        
-        accuracy = history.history['val_acc'][-1]
-        print("ACCURACY: ", accuracy)
-        total+= accuracy
+    model.add(GRU(100+n_labels, return_sequences=True, input_shape=(time_steps, n_features)))
+    model.add(GRU(100+n_labels))
+    model.add(Dense(n_labels, activation='softmax'))
+    model.compile(loss='sparse_categorical_crossentropy',optimizer='adam', metrics=['acc'])
+    model.summary()
 
-    print("AVG: ", 1. * total / FOLDS)
-    return 1. * total / FOLDS
+    return model  
+
+#########################################################
+###### USE RNN TO CLASSIFY PACKET SEQUENCES -> SNI ######
+#########################################################
+
+def DLClassification(X_train, X_test, y_train, y_test,time_steps, n_features, n_labels):
+    X_train = np.reshape(X_train, (np.shape(X_train)[0], np.shape(X_train)[1], n_features))
+    X_test = np.reshape(X_test, (np.shape(X_test)[0], np.shape(X_test)[1], n_features))
+
+    history = History()
+
+    model = create_model(time_steps, n_features, n_labels)
+    model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=1, shuffle=False, validation_data=(X_test, y_test), callbacks = [history])
+
+    accuracy = history.history['val_acc'][-1]
+    return accuracy
+  
+#***********************************************************************************
+# autosklearn classifier to find the best achievable accuracy
+#***********************************************************************************
+def auto_sklearn_classification(X_train, X_test, y_train, y_test):
+  cls = autosklearn.classification.AutoSklearnClassifier(time_left_for_this_task=300, per_run_time_limit=90, ml_memory_limit=50000)
+  cls.fit(X_train, y_train)
+  print(cls.sprint_statistics())
+  print(cls.show_models())
+  predictions = cls.predict(X_test)
+  accuracy = accuracy_score(y_test, predictions)
+  return accuracy
 
 
 if __name__ == "__main__":
     # run once w/min connections = 100 as in paper
-    DLClassification("training/GCDay1seq100.csv", 100)
+    datasetfile = "training/GCDay1seq100.csv"
+    min_connections_to_try = [25, 50, 75, 100, 125, 150, 175, 200, 225, 250]
+
+    kf = KFold(n_splits=FOLDS, shuffle=True)
+    total_nn, total_cls = 0, 0
 
     # try a variety of min conn settings for graph
-    min_connections_to_try = [25, 50, 75, 100, 125, 150, 175, 200, 225, 250]
     accuracies = []
     for min_connections in min_connections_to_try:
-        accuracies.append(DLClassification("training/GCDay1seq100.csv", min_connections))
+        X, y, time_steps, n_features, n_labels = data_load_and_filter(datasetfile, min_connections)
+        for train_index, test_index in kf.split(X):
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            
+            nn_acc = DLClassification(X_train, X_test, y_train, y_test,time_steps, n_features, n_labels)
+            print("Neural Net ACCURACY: %s"%(nn_acc))
+            
+            cls_acc = auto_sklearn_classification(X_train, X_test, y_train, y_test)
+            print("Auto sklearn Accuracy: %s "%(cls_acc))
+            
+            total_nn+= nn_acc
+            total_cls += cls_acc
 
+
+        total_nn, total_cls = 1. * total_nn / FOLDS, 1. * total_cls / FOLDS 
+        print("AVG Neural Net: %s, AVF CLS: %s "%(total_nn, total_cls))
+  
+        accuracies.append([total_nn, total_cls])
+
+    '''
     plt.plot(min_connections_to_try, accuracies)
     plt.xlabel("Mininimum Connections")
     plt.ylabel("Accuracy")
     plt.show()
+    '''
+    
+    print(accuracies)
 
 
